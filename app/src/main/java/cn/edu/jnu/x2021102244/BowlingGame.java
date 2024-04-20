f11af9beeedc6b26c8774fef10a51c78d1db9cf7
AAArisle <2416948889@qq.com>
2024-04-20 17:08:03 +0800

    测试三，2，0，1，9，16个0，测试通过

--- app/src/main/java/cn/edu/jnu/x2021102244/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2021102244/BowlingGame.java
@@ -1,13 +1,26 @@
 package cn.edu.jnu.x2021102244;
 
 public class BowlingGame {
-    private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            // 补中奖励
+            if (rolls[frameIndex] + rolls[frameIndex + 1] == 10)
+            {
+                score += 10 + rolls[frameIndex + 2];
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+            }
+            frameIndex += 2;
+        }
         return score;
     }
 }
