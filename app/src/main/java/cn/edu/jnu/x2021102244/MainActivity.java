14ba90aa3e8df7ef31af7e5a7a7115dfd0016a3a
AAArisle <2416948889@qq.com>
2024-04-20 17:02:41 +0800

    建立项目

---
+++ app/src/main/java/cn/edu/jnu/x2021102244/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.jnu.x2021102244;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
