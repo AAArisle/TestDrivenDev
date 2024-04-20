cc7d3df5b14275532ce3e294fac519f6652cff63
AAArisle <2416948889@qq.com>
2024-04-20 17:06:12 +0800

    测试二，20个1

--- app/src/test/java/cn/edu/jnu/x2021102244/BowlingGameTest.java
+++ app/src/test/java/cn/edu/jnu/x2021102244/BowlingGameTest.java
@@ -1,14 +1,29 @@
 package cn.edu.jnu.x2021102244;
 import junit.framework.TestCase;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest extends TestCase {
+
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void test20Zeros() {
-        BowlingGame game = new BowlingGame();
         for (int i=0; i<20; i++)
             game.roll(0);
-        assertEquals(0,game.score());
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void test20Ones() {
+        for (int i=0; i<20; i++)
+            game.roll(1);
+        assertEquals(20, game.score());
     }
 }
